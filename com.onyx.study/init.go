462d83856ec7e23b1aaea6b99cb8520a4ff4d7d6
onyx <changhe626@163.com>
2018-10-31 11:03:23 +0800

    2018年10月31日11:03:15

--- com.onyx.study/init.go
+++ com.onyx.study/init.go
@@ -10,23 +10,19 @@ import (
 var Pi float64
 
 //先执行的init这个函数
-func init(){
+func init() {
 
 	Pi = 4 * math.Atan(1) // init() function computes Pi
 }
 
-func main(){
+func main() {
 	var goos string = runtime.GOOS
 	fmt.Printf("The operating system is: %s\n", goos)
 	path := os.Getenv("PATH")
 	fmt.Printf("Path is %s\n", path)
 
-	fmt.Println("hello world",90)
-
+	fmt.Println("hello world", 90)
 
 	fmt.Println(Pi)
 
-
 }
-
-
